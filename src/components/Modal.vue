46cfd3aaa823e772ece84ff021d074ecc3b6fb96
Rosendo Rodriguez <rorodriguez@luxoft.com>
2022-03-18 10:54:15 -0600

    Changes added

---
+++ src/components/Modal.vue
@@ -0,0 +1,36 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'modal'
+}
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  z-index: 100;
+  position: fixed;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  .modal-content {
+    border-radius: 20px;
+    padding: 48px 32px;
+    max-width: 450px;
+    background-color: #252945;
+    color: #fff;
+    p {
+      text-align: center;
+    }
+    .actions {
+      margin-top: 24px;
+      button {
+        flex: 1;
+      }
+    }
+  }
+}
+</style>
